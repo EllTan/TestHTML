380db9030684fdc213e7dde3d2c76d541afe3217
Dmitry <byelltanrb@gmail.com>
2021-08-16 23:32:33 +0300

    9

--- TestHTML.py
+++ TestHTML.py
@@ -13,16 +13,8 @@ async def read_items():
         <!DOCTYPE html>
         <html lang="ru">
         <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Дмитрий Жарков - разработчик</title>
-            <link
-            href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap&subset=cyrillic"
-            rel="stylesheet"
-            />
-            <link rel="stylesheet" href="css/style.css" />
-            <link rel="stylesheet" href="/css/animate.css" />
-            <link rel="stylesheet" href="css/swiper-bundle.min.css" />
+            <meta charset="UTF-8" />            
+            <title>Дмитрий Жарков - разработчик</title>            
         </head>
         <body>
         <header class="hero">
